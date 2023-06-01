29c9911a844f8227d80457f490bffa78946cf7bf
M0ngi <saidanemongi@gmail.com>
2023-06-01 10:30:50 +0100

    Add create quiz DTO Class validator

--- backend/src/quiz/dto/create-quiz.dto.ts
+++ backend/src/quiz/dto/create-quiz.dto.ts
@@ -1,17 +1,28 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsDate, IsNotEmpty, IsString, IsUUID } from 'class-validator';
 import { CreateQuizQuestionDto } from 'src/quiz-question/dto/create-quiz-question.dto';
 
 export class CreateQuizDto {
   @IsNotEmpty()
+  @IsString()
   title: string;
+
   @IsNotEmpty()
+  @IsString()
   description: string;
+
   @IsNotEmpty()
+  @IsDate()
   scheduleDate: Date;
+
   @IsNotEmpty()
+  @IsDate()
   deadlineDate: Date;
+
   @IsNotEmpty()
   questions: CreateQuizQuestionDto[];
 
+  @IsNotEmpty()
+  @IsString()
+  @IsUUID()
   chapterId: string;
 }
